056c1a7f70aedebefe4f3aca5d154c0f14d816c4
Arya Deshmukh <aryadeshmukh12@gmail.com>
2024-07-03 10:52:24 +0530

    write functional tests for register and login

--- app.py
+++ app.py
@@ -1,56 +1,76 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
-from werkzeug.security import generate_password_hash, check_password_hash
+# This module contains the server implmentation of Course Website Merger
 
-USERS_DB = 'users.db'
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from exceptions import InvalidCredentials, InvalidUsername
+from constants import USERS_DB
+from database import get_db_connection, initialize_users_db
 
 app = Flask(__name__)
 
-def get_db_connection(db_file: str):
-    '''
-    Function to get a database connection
-    
-    Keyword arguments:
-    db_file -- .db file representing the database
-    '''
-    con = sqlite3.connect(db_file)
-    con.row_factory = sqlite3.Row
-    return con
-
-# Create the users table if it doesn't exist.
-with get_db_connection(USERS_DB) as con:
-    con.execute('''CREATE TABLE IF NOT EXISTS users
-                    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     username TEXT UNIQUE,
-                     hashed_password TEXT)''')
-    con.commit()
+initialize_users_db(erase=False)
 
 @app.route('/')
 def home():
     return 'Success!'
 
+def _register(username: str, password: str) -> None:
+    '''
+    Creates a new account for the user and stores credentials in users database.
+
+    If entered username is already associated with an account, an InvalidUsername exception is raised.
+    
+    Keyword arguments:
+    username -- entered username
+    password -- entered password
+    '''
+    hashed_password = generate_password_hash(password)
+    with get_db_connection(USERS_DB) as con:
+        if con.execute('SELECT username FROM users WHERE username = ?', (username,)).fetchone():
+            raise InvalidUsername
+        else:
+            con.execute('INSERT INTO users (username, hashed_password) VALUES (?, ?)',
+                        (username, hashed_password))
+            con.commit()
+
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     '''
-    Creates a new account for the user.
+    Endpoint for user to create new account.
 
-    If entered username is already in use, user must choose a different username.
-    Password is hashed before being stored in the database for security purposes.
+    For posts, entered username and password are passed into _register function.
+    Error message is returned if _register function raises InvalidUsername error.
+    For gets, screen for user to register is shown.
     '''
     error = None
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        hashed_password = generate_password_hash(password)
-        with get_db_connection(USERS_DB) as con:
-            if con.execute('SELECT username FROM users WHERE username = ?', (username,)).fetchone():
-                error = 'Username already in use. Please try again.'
-            else:
-                con.execute('INSERT INTO users (username, hashed_password) VALUES (?, ?)',
-                            (username, hashed_password))
-                con.commit()
-                return redirect(url_for('login'))
+        try:
+            username = request.form['username']
+            password = request.form['password']
+            _register(username, password)
+            return redirect(url_for('login'))
+        except InvalidUsername:
+            error = 'Username already in use. Please try again.'
     return render_template('register.html', error=error)
+
+def _login(username: str, password: str) -> None:
+    '''
+    Logs user in based on entered credentials.
+
+    If entered username does not correspond to any account, an InvalidUsername exception is raised.
+    If entered password is incorrect for entered username, an InvalidCredentials exception is raised.
+
+    Keyword arguments:
+    username -- entered username
+    password -- entered password
+    '''
+    with get_db_connection(USERS_DB) as con:
+        user = con.execute('SELECT hashed_password FROM users WHERE username = ?', (username,)).fetchone()
+        if user is None:
+            raise InvalidUsername
+        elif not check_password_hash(user['hashed_password'], password):
+            raise InvalidCredentials
+        
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -64,14 +84,13 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        with get_db_connection(USERS_DB) as con:
-            user = con.execute('SELECT hashed_password FROM users WHERE username = ?', (username,)).fetchone()
-            if user is None:
-                error = 'No account associated with username. Please try again.'
-            elif not check_password_hash(user['hashed_password'], password):
-                error = 'Username or password is incorrect. Please try again.'
-            else:
-                return redirect(url_for('home'))
+        try:
+            _login(username, password)
+            return redirect(url_for('home'))
+        except InvalidUsername:
+            error = 'No account associated with username. Please try again.'
+        except InvalidCredentials:
+            error = 'Username or password is incorrect. Please try again.'
     return render_template('login.html', error=error)
 
 if __name__ == '__main__':
---
+++ database.py
@@ -0,0 +1,33 @@
+# Module containing all database handling functions.
+
+import sqlite3
+from constants import USERS_DB
+
+def get_db_connection(db_file: str):
+    '''
+    Function to get a database connection
+    
+    Keyword arguments:
+    db_file -- .db file representing the database
+    '''
+    con = sqlite3.connect(db_file)
+    con.row_factory = sqlite3.Row
+    return con
+
+def initialize_users_db(erase=False) -> None:
+    '''
+    Creates users database containing user credential information if it does not already exist.
+
+    If erase is set to true then the existing database is deleted a brand new one is created.
+
+    Keyword arguments:
+    erase -- flag indicating whether the database should be erased
+    '''
+    with get_db_connection(USERS_DB) as con:
+        if erase:
+            con.execute('DROP TABLE IF EXISTS users')
+        con.execute('''CREATE TABLE IF NOT EXISTS users
+                        (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            username TEXT UNIQUE,
+                            hashed_password TEXT)''')
+        con.commit()
